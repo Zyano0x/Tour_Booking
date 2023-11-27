88d89aad25935d7f971d11b04ec3f7c61fc6e71b
Zyano <nthdm00@outlook.com>
2023-11-27 19:24:30 +0700

    Implemented Authorization JWT & Some Stuffs

---
+++ src/main/resources/static/assets/admin/js/pages/login.js
@@ -0,0 +1,35 @@
+document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let email = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+
+    login(email, password);
+});
+
+function login(email, password) {
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    let raw = JSON.stringify({
+        email: email,
+        password: password,
+    });
+
+    let requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+    };
+
+    fetch("/api/auth/login", requestOptions)
+        .then((response) => {
+            if (response.ok) return response.json();
+            else throw new Error("Error: " + response.statusText);
+        })
+        .then((result) => {
+            window.location.href = "/panel";
+        })
+        .catch((error) => console.error("Login Error:", error.message));
+}
