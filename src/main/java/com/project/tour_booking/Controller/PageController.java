4bae6d88b28ea144ca7050c065169c3a4d39f2a8
Pham Quoc Duy <93472512+phamquocduy201@users.noreply.github.com>
2023-12-14 22:21:16 +0700

    Update PageController.java

--- src/main/java/com/project/tour_booking/Controller/PageController.java
+++ src/main/java/com/project/tour_booking/Controller/PageController.java
@@ -1,8 +1,16 @@
 package com.project.tour_booking.Controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.project.tour_booking.Entity.User;
+import com.project.tour_booking.Service.User.UserService;
 
 import lombok.AllArgsConstructor;
 
@@ -10,6 +18,8 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/")
 @AllArgsConstructor
 public class PageController {
+    private UserService userService;
+
     @GetMapping
     public String home() {
         return "user/index";
@@ -23,5 +33,26 @@ public class PageController {
     @GetMapping("tours/{tourId}")
     public String detail() {
         return "user/tour_details";
+    }
+
+    @GetMapping("/cart")
+    public String cart(Model model) {
+        return "user/cart";
+    }
+
+    @ModelAttribute("userId")
+    public Long getUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            User user = userService.user(userDetails.getUsername());
+
+            if (user != null) {
+                return user.getId();
+            }
+        }
+
+        return null;
     }
 }
