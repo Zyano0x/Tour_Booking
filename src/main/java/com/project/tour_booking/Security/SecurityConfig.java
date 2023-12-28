247841ada5dd519accb22d2ae15d303720b3820f
Zyano0x <20568836+Zyano0x@users.noreply.github.com>
2023-12-28 10:19:50 +0700

    Updated Admin Side & Fixed Articles Service

--- src/main/java/com/project/tour_booking/Security/SecurityConfig.java
+++ src/main/java/com/project/tour_booking/Security/SecurityConfig.java
@@ -19,36 +19,30 @@ import org.springframework.security.web.authentication.logout.LogoutHandler;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-        private static final String[] WHITE_LIST = { "/assets/**", "/api/**", "/panel/*", "/tours/*", "/articles/*",
-                        "/*", };
+    private static final String[] WHITE_LIST = {"/assets/**", "/api/**", "/panel/*", "/tours/*", "/articles/*", "/*",};
 
-        private static final String[] BLACK_LIST = { "/api/admin/**", "/panel/**", };
+    private static final String[] BLACK_LIST = {"/api/admin/**", "/panel/**",};
 
-        private final JWTAuthenticationFilter jwtAuthenticationFilter;
-        private final JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-        private final AuthenticationProvider authenticationProvider;
-        private final LogoutHandler logoutHandler;
+    private final JWTAuthenticationFilter jwtAuthenticationFilter;
+    private final JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final AuthenticationProvider authenticationProvider;
+    private final LogoutHandler logoutHandler;
 
-        @Bean
-        public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
-                http
-                                .csrf(AbstractHttpConfigurer::disable)
-                                .authorizeHttpRequests(request -> request
-                                                .requestMatchers(WHITE_LIST).permitAll()
-                                                .requestMatchers(BLACK_LIST).hasAnyAuthority(Role.ADMIN.name())
-                                                .anyRequest().authenticated())
-                                .sessionManagement(session -> session
-                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                                .authenticationProvider(authenticationProvider)
-                                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                                .exceptionHandling(exception -> exception
-                                                .authenticationEntryPoint(jwtAuthenticationEntryPoint))
-                                .logout(logout -> logout
-                                                .logoutUrl("/api/auth/logout")
-                                                .addLogoutHandler(logoutHandler)
-                                                .logoutSuccessHandler(((request, response,
-                                                                authentication) -> SecurityContextHolder
-                                                                                .clearContext())));
-                return http.build();
-        }
+    @Bean
+    public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(WHITE_LIST).permitAll()
+                        .requestMatchers(BLACK_LIST).hasAnyAuthority(Role.ADMIN.name())
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .logout(logout -> logout.logoutUrl("/api/auth/logout")
+                        .addLogoutHandler(logoutHandler)
+                        .logoutSuccessHandler(((request, response, authentication) -> SecurityContextHolder.clearContext())));
+        return http.build();
+    }
 }
